9004ef7216d8b07d1d7d9c58b9282e285c7c148d
vannakskb <vannakchhit@findod.com>
2021-05-23 04:22:37 +0700

    Hooking up FilePond to signed uploads

--- src/components/AppUploader.vue
+++ src/components/AppUploader.vue
@@ -5,10 +5,50 @@
 </template>
 
 <script>
+import axios from 'axios'
 import * as Filepond from 'filepond'
 export default {
     mounted(){
-        const pond = Filepond.create(this.$refs.file)
+        const pond = Filepond.create(this.$refs.file,{
+            allowRevert: false,
+            server:{
+                process: (fieldName,file, metadata, load, error, progress, abort) => {
+                    console.log(fieldName,file, metadata, load, error, progress, abort);
+                    let form = new FormData()
+                    axios.post('/api/files/signed', {
+                        name: metadata.fileInfo.name,
+                        extension: metadata.fileInfo.extension,
+                        size: metadata.fileInfo.size
+                    }).then((response) => {
+                       
+                        file.additionalData = response.data.additionalData
+
+                        for(var field in file.additionalData){
+                            form.append(field, file.additionalData[field])
+                        }   
+                        form.append('file',file)
+                        
+                        axios.post(response.data.attributes.action, form,{
+                            onUploadProgress (e) {
+                                progress(e.lengthComputable,e.loaded, e.total)
+                            }
+                        }).then(() => {
+                            load(`${file.additionalData.key}`)
+                        })
+                    })
+                }
+            },
+            onaddfile: (error, file) => {
+                if(error){
+                    return
+                }
+                file.setMetadata('fileInfo',{
+                    name: file.filenameWithoutExtension,
+                    extension: file.fileExtension,
+                    size: file.fileSize
+                })
+            }
+        })
         console.log(pond);
     }
 }
